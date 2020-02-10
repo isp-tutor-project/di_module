1ecc766f8e1ec62031ff8de209449cc3dffd9277
Scott Silliman <scott.t.silliman@gmail.com>
2020-02-10 00:04:34 -0500

    latest config changes

--- .eleventy.js
+++ .eleventy.js
@@ -8,11 +8,35 @@ function prettify(content, outputPath) {
     return content;
 }
 
+
+
+function verticallyCenter(content, pctFromTop=30) {
+    return `
+        <div class="dev centered-wrapper">
+            <div class="dev centered" style="top:${pctFromTop}%;">
+                ${content}
+            </div>
+        </div>
+    `;
+}
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addTransform("prettier", prettify);
-    eleventyConfig.addWatchTarget("dist/*.js");
-    eleventyConfig.addPassthroughCopy({"dist/*.*": "."});
+
+    eleventyConfig.addWatchTarget("./dist/");
+    eleventyConfig.addWatchTarget("./src/js/");
+    
+    eleventyConfig.addPassthroughCopy({"dist": "."});
     eleventyConfig.addPassthroughCopy({"src/img": "img"});
+    
+    eleventyConfig.addPairedShortcode("vertcenter", verticallyCenter);
+    // eleventyConfig.addNunjucksShortcode("columnwidths", function (lhsWidth, rhsWidth) {
+    //     return `
+    //     {% block lhsWidth %} style="width:${lhsWidth}%;" {% endblock %}
+    //     {% block rhsWidth %} style="width:${rhsWidth}%;" {% endblock %}
+    // `;
+    // });
+
     return {
         dir: {
             input: "src/templates",
